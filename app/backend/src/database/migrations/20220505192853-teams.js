60b66f053f1644e9e5b8afa0043814a02b08d488
isadora <isadora.bayma@gmail.com>
2022-05-05 16:34:14 -0300

    create migration Teams

---
+++ app/backend/src/database/migrations/20220505192853-teams.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
