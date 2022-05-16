c877598738ed3910c496d27a68c97149dac67af5
isadora <isadora.bayma@gmail.com>
2022-05-16 02:42:30 -0300

    req4

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -7,6 +7,8 @@ import { describe, it, before, after } from 'mocha';
 // import { app } from '../app';
 import User from '../database/models/User';
 import LoginRepo from '../repository/login';
+import LoginService from '../service/login';
+import LoginController from '../controller/login';
 
 // import { Response } from 'superagent';
 
@@ -17,27 +19,48 @@ const { expect } = chai;
 // let chaiHttpResponse: Response;
 
 describe('LoginModel', () => {
-  const mockUser = {
+  const mockFullUser = {
     id: 1,
     username: 'Admin',
     role: 'admin',
     email: 'admin@admin.com',
-    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  }
+
+  const mockUser = {
+    id: 1,
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com'
   }
 
   describe('LoginRepo', () => {
     beforeEach(sinon.restore);
     it('db findUser fail', () => {
       sinon.stub(User, 'findOne').rejects();
-      // expect(LoginRepo.findUser(mockUser.email)).to.eventually.be.rejected;
-      expect(LoginRepo.findUser(mockUser.email)).to.eventually.be.equal(null);
+      expect(LoginRepo.findUser(mockFullUser.email)).to.eventually.be.equal(null);
     }),
     it('db findUser ok', () => {
-      sinon.stub(User, 'findOne').resolves(mockUser as User);
-      expect(LoginRepo.findUser(mockUser.email))
-      .to.eventually.be.equal(mockUser);
+      sinon.stub(User, 'findOne').resolves(mockFullUser as User);
+      expect(LoginRepo.findUser(mockFullUser.email))
+      .to.eventually.be.equal(mockFullUser);      
     })
   })
 
-  // describe('Login')
+  // describe('LoginService', () => {
+  //   it('findUser ok', () => {
+  //     expect(LoginService.findUser(mockFullUser)).to.equal(mockUser);
+  //   })
+  // })
+
+  // describe('LoginContrller', () => {
+  //   beforeEach(sinon.restore);
+  //   it('login no email', () => {
+  //     sinon.stub(User, 'findOne').rejects();
+  //     expect(LoginController.login())
+  //   }),
+  //   it('login wrong password'),
+  //   it('tokenGenerator'),
+  //   it('login ok')
+  // })  
 })
