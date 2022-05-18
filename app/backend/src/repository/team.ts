70de1610d59786a53a82230fc2e29e6238180543
isadora <isadora.bayma@gmail.com>
2022-05-17 22:43:28 -0300

    team repo

---
+++ app/backend/src/repository/team.ts
@@ -0,0 +1,20 @@
+import { Iteam } from '../database/interfaces/Iteam';
+import Team from '../database/models/Team';
+
+export default class TeamRepo {
+  public static async getAll(): Promise<Iteam[] | null> {
+    const teamList = await Team.findAll;
+
+    if (!teamList) return null;
+
+    return teamList as Iteam[];
+  }
+
+  public static async getAllById(id: number): Promise<Iteam | null> {
+    const team = await Team.findOne({ where: { id } });
+
+    if (!team) return null;
+
+    return team as Iteam;
+  }
+}
