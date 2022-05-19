07e939428c1d7ddcbf6fdff305908652a10b77ca
isadora <isadora.bayma@gmail.com>
2022-05-19 20:08:39 -0300

    matches controller

--- app/backend/src/controller/match.ts
+++ app/backend/src/controller/match.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response, Request } from 'express';
 import MatchService from '../service/match';
 
 export default class MatchController {
-  public static async getall(req: Request, res: Response, next: NextFunction): Promise<
+  public static async getAll(req: Request, res: Response, next: NextFunction): Promise<
   Response | void> {
     try {
       const matchList = await MatchService.getAll();
