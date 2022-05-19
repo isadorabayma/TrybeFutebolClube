4f685224a2f41d0f693007a2260a9b25ebbe0207
isadora <isadora.bayma@gmail.com>
2022-05-19 10:54:26 -0300

    fixing test

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -19,44 +19,42 @@ const { expect } = chai;
 let chaiHttpResponse: Response;
 
 describe('/teams', () => {
-  // const mockTeamList = {
-  //   [
-  //     {
-  //       "id": 1,
-  //       "teamName": "Avaí/Kindermann"
-  //     },
-  //     {
-  //       "id": 2,
-  //       "teamName": "Bahia"
-  //     },
-  //     {
-  //       "id": 3,
-  //       "teamName": "Botafogo"
-  //     }
-  //   ]
-  // }
+  const mockTeamList = [
+      {
+        id: 1,
+        teamName: "Avaí/Kindermann"
+      },
+      {
+        id: 2,
+        teamName: "Bahia"
+      },
+      {
+        id: 3,
+        teamName: "Botafogo"
+      }
+  ]
 
   describe('integration of /teams', () => {
     beforeEach(sinon.restore);
-    // it('getAll working', async () => {
-    //   sinon.stub(Team, 'getAll').resolves(mockTeamList as Team);
-    //   chaiHttpResponse = await chai.request(app).get('/teams')
+    it('getAll working', async () => {
+      sinon.stub(Team, 'findAll').resolves(mockTeamList as [Team]);
+      chaiHttpResponse = await chai.request(app).get('/teams')
 
-    //   expect(chaiHttpResponse.status).to.be.equal(200);
-    //   expect(chaiHttpResponse.body[0]).have.property('id');
-    //   expect(chaiHttpResponse.body[0]).have.property('teamName');
-    // }),
-    // it('integration of /teams/:id', () => {
-    //   sinon.stub(Team, 'getAllById').resolves(mockTeamList as Team);
-    //   chaiHttpResponse = await chai.request(app).get('/teams/1')
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body[0]).have.property('id');
+      expect(chaiHttpResponse.body[0]).have.property('teamName');
+    }),
+    it('integration of /teams/:id', async () => {
+      sinon.stub(Team, 'findOne').resolves(mockTeamList[0] as Team);
+      chaiHttpResponse = await chai.request(app).get('/teams/1')
 
-    //   expect(chaiHttpResponse.status).to.be.equal(200);
-    //   expect(chaiHttpResponse.body).have.property('id');
-    //   expect(chaiHttpResponse.body).have.property('teamName');
-    //   expect(chaiHttpResponse.body.id).to.be.equal(1);
-    // }),
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).have.property('id');
+      expect(chaiHttpResponse.body).have.property('teamName');
+      expect(chaiHttpResponse.body.id).to.be.equal(1);
+    }),
     it('getAll fail', () => {
-      sinon.stub(Team, 'getAll').rejects();
+      sinon.stub(Team, 'findAll').rejects();
       expect(TeamRepo.getAll()).to.eventually.be.equal(null);
     }),
     it('')
